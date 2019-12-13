f93bab777b7597cd5e6595e7ce69ab75ace259f4
Puneeth1996 <38997799+Puneeth1996@users.noreply.github.com>
2019-12-13 16:47:11 +0530

    Initial https://www.grocerycrud.com/documentation/options_functions/

--- application/controllers/LearnGroceryCrud.php
+++ application/controllers/LearnGroceryCrud.php
@@ -1,14 +1,11 @@
 <?php 
     class LearnGroceryCrud extends CI_Controller {  
-
         public function index() { 
             $this->load->view('test');  
-        } 
-
+        }
         public function hello() { 
             echo "This is hello function.234346456456"; 
         }
-
         public function fishes() { 
             echo "Find a Nemo"; 
         }
